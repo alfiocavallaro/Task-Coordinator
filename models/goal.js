d595b3c4cf0b4b2d936dcdd9236a6f074fa3aa69
Alfio Cavallaro <alfio.cavallaro@gmail.com>
2016-01-02 12:30:05 +0100

    update

--- models/goal.js
+++ models/goal.js
@@ -1,7 +1,7 @@
 // Load required packages
 var mongoose = require('mongoose');
 
-// Define our beer schema
+// Define our goal schema
 var GoalSchema = new mongoose.Schema({
   trigger: String,
   action: String,
